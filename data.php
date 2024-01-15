31cb10a74a490c9f9ba552a78e109321375c72fc
FrancescoMazzocchi <123@email.it>
2024-01-15 17:55:58 +0100

    improve layout

--- data.php
+++ data.php
@@ -7,8 +7,10 @@ require_once __DIR__ . '/Models/toys.php';
 
 
 $prodotti = [
-    new Food('crocchette premium', 50, 'cane', true, 'https://shop-cdn-m.mediazs.com/bilder/prezzo/speciale/kg/rocco/mealtime/crocchette/per/cani/7/800/rocco_mealtime_lamb_1kg_1000x1000_7.jpg', 'dry', '550', '4',),
-    new Cuccia('cuccia kingsize', 120, 'cat', false, 'https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/251962edb7de5760e0b55659df48bbfe.jpg?imageView2/2/w/800/q/70', 50, 'red', 1.5),
-    new Toys('sonaglio', 5, 'dog', true, 'https://qz-petshop.com/11245-large_default/camon-gioco-per-cani-osso-in-tpr-con-sonaglio-vari-colori.jpg', 15, 'plastic', 'green', 0.3)
-
+    new Food('crocchette premium', 12.5, 'cane', true, 'https://shop-cdn-m.mediazs.com/bilder/prezzo/speciale/kg/rocco/mealtime/crocchette/per/cani/7/800/rocco_mealtime_lamb_1kg_1000x1000_7.jpg', 'dry', '550', '4',),
+    new Cuccia('cuccia kingsize', 120, 'gatto', false, 'https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/251962edb7de5760e0b55659df48bbfe.jpg?imageView2/2/w/800/q/70', 50, 'arancione', 1.5),
+    new Toys('sonaglio', 5, 'cane', true, 'https://qz-petshop.com/11245-large_default/camon-gioco-per-cani-osso-in-tpr-con-sonaglio-vari-colori.jpg', 15, 'plastica', 'blu', 0.3),
+    new Food('crocchette premium', 12.5, 'cane', true, 'https://shop-cdn-m.mediazs.com/bilder/prezzo/speciale/kg/rocco/mealtime/crocchette/per/cani/7/800/rocco_mealtime_lamb_1kg_1000x1000_7.jpg', 'dry', '550', '4',),
+    new Cuccia('cuccia kingsize', 120, 'gatto', false, 'https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/251962edb7de5760e0b55659df48bbfe.jpg?imageView2/2/w/800/q/70', 50, 'arancione', 1.5),
+    new Toys('sonaglio', 5, 'cane', true, 'https://qz-petshop.com/11245-large_default/camon-gioco-per-cani-osso-in-tpr-con-sonaglio-vari-colori.jpg', 15, 'plastica', 'blu', 0.3)
 ];
--- index.php
+++ index.php
@@ -11,12 +11,13 @@ require_once __DIR__ . '/data.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>pet store</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
     <div class="container">
         <div class="row">
-            <h2>Prodotti</h2>
+            <h2 class="mb-5">Prodotti</h2>
         </div>
         <div class="row">
             <?php foreach ($prodotti as $prodotto) : ?>
@@ -27,11 +28,15 @@ require_once __DIR__ . '/data.php';
                             <h5 class="card-title">
                                 <?= $prodotto->name ?>
                             </h5>
-                            Prezzo: <?= $prodotto->price . " Euro" ?> <br>
-                            <?= (isset($prodotto->color)) ? "(" . $prodotto->color . ")" : "" ?>
                             <h6 class="card-subtitle mb-2 text-muted">
+                                <?= $prodotto->category ?>
+                                <?php if ($prodotto->category == 'cane') { ?> <i class="fa-solid fa-dog"></i> <?php } ?>
+                                <?php if ($prodotto->category == 'gatto') { ?> <i class="fa-solid fa-cat"></i> <?php } ?>
 
                             </h6>
+                            Prezzo: <?= $prodotto->price . " €" ?> <br>
+                            <?= (isset($prodotto->color)) ? "Colore: " . $prodotto->color : "" ?>
+
                             <!-- descrizione prodotto da aggiungere  -->
                         </div>
                     </div>
