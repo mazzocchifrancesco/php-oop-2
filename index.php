c2aaac6523d0590ea095a02653020cbd27bd6bf0
FrancescoMazzocchi <123@email.it>
2024-01-15 19:29:39 +0100

    add styles + categoria con funzione

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ require_once __DIR__ . '/data.php';
     <title>pet store</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
@@ -22,20 +23,25 @@ require_once __DIR__ . '/data.php';
         <div class="row">
             <?php foreach ($prodotti as $prodotto) : ?>
                 <div class="col-3">
-                    <div class="card">
+                    <div class="card mb-3 overflow-y-auto">
                         <img src="<?= $prodotto->imgUrl ?>" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">
                                 <?= $prodotto->name ?>
                             </h5>
                             <h6 class="card-subtitle mb-2 text-muted">
-                                <?= $prodotto->category ?>
-                                <?php if ($prodotto->category == 'cane') { ?> <i class="fa-solid fa-dog"></i> <?php } ?>
-                                <?php if ($prodotto->category == 'gatto') { ?> <i class="fa-solid fa-cat"></i> <?php } ?>
-
+                                <?= $prodotto->category . ", " . get_class($prodotto) ?>
+                                <?php if ($prodotto->category == 'cane') {
+                                    echo '<i class="fa-solid fa-dog"></i>';
+                                } ?>
+                                <?php if ($prodotto->category == 'gatto') {
+                                    echo '<i class="fa-solid fa-cat"></i>';
+                                }; ?>
                             </h6>
                             Prezzo: <?= $prodotto->price . " €" ?> <br>
+                            Disponibilità: <?= $prodotto->availability ? '<i class="fa-solid fa-check"></i>' : '<i class="fa-solid fa-x"></i>' ?><br>
                             <?= (isset($prodotto->color)) ? "Colore: " . $prodotto->color : "" ?>
+                            Peso: <?= $prodotto->weight . " kg" ?>
 
                             <!-- descrizione prodotto da aggiungere  -->
                         </div>
