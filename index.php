0b852e59f3352b1bee1a2eca7f9752cde1e76213
FrancescoMazzocchi <123@email.it>
2024-01-15 17:25:35 +0100

    inizio di stilizzazione cards

--- index.php
+++ index.php
@@ -1,5 +1,46 @@
 <?php
 
 require_once __DIR__ . '/data.php';
+?>
 
-var_dump($prodotti);
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pet store</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <h2>Prodotti</h2>
+        </div>
+        <div class="row">
+            <?php foreach ($prodotti as $prodotto) : ?>
+                <div class="col-3">
+                    <div class="card">
+                        <img src="<?= $prodotto->imgUrl ?>" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                <?= $prodotto->name ?>
+                            </h5>
+                            Prezzo: <?= $prodotto->price . " Euro" ?> <br>
+                            <?= (isset($prodotto->color)) ? "(" . $prodotto->color . ")" : "" ?>
+                            <h6 class="card-subtitle mb-2 text-muted">
+
+                            </h6>
+                            <!-- descrizione prodotto da aggiungere  -->
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+</html>
